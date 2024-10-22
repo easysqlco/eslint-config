d479ab0e6b93891f49400f1677d0a890eee5327b
Metehan Senol <metehansenol@gmail.com>
2024-10-22 22:24:15 +0300

    add tseslint typechecked

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { ignores: ['node_modules', 'dist', 'public'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { ignores: ['node_modules', 'dist', 'public', '**/*.d.ts'] },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    ...tseslint.configs.disableTypeChecked,
+  },
   eslintPluginPrettierRecommended,
   {
     rules: {
@@ -22,7 +33,19 @@ export default [
           tabWidth: 2,
         },
       ],
-      'no-console': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-console': 'warn',
     },
   },
 ];
