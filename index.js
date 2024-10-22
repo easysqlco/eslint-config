62e0774fe34de8794a0b870332b6f39f29b62627
Metehan Senol <metehansenol@gmail.com>
2024-10-23 00:06:47 +0300

    use tsconfig.eslint.json for linting

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export default [
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        project: './tsconfig.eslint.json',
         tsconfigRootDir: import.meta.dirname,
       },
     },
